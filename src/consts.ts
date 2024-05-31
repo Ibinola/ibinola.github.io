ef1e61185432d61df6eb074280a348466996679b
Michael Ibinola <ibinolamichael1@gmail.com>
2024-05-31 20:09:36 +0100

    feat: added projects page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Michael Ibinola';
-export const SITE_DESCRIPTION = 'Frontend Engineer';
+export const SITE_TITLE = "Michael Ibinola";
+export const SITE_DESCRIPTION = "Frontend Engineer";
+
+export const projects = [
+  {
+    name: "that/'swhatshebot",
+    url: "https://github.com/Ibinola/thatswhatshebot",
+    description:
+      "Discord bot serving random quotes from the tv-show (The Office)",
+  },
+];
