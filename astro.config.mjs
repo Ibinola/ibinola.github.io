e109e9ed91ed989db424612af81ec8e111923b78
Michael Ibinola <ibinolamichael1@gmail.com>
2024-08-31 18:27:54 +0100

    add nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ibinola.github.io',
-  // integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react()],
 });
